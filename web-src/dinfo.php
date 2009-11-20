730bde8b63b2e8237ca598801146e5ee4586e35d
fayjf <fayjf@7f0cb862-5218-0410-a997-914c9d46530a>
2009-11-20 04:29:55 +0000

    Creating freeglut 2.6.0 Release Candidate 3
    git-svn-id: https://svn.code.sf.net/p/freeglut/code/tags/FG_2_6_0_RC3@852 7f0cb862-5218-0410-a997-914c9d46530a

---
+++ web-src/dinfo.php
@@ -0,0 +1,11 @@
+<?php
+require("template.php");
+
+# Now set the title of the page:
+setPageTitle("Info for Developers");
+
+# Make the header.
+generateHeader($_SERVER['PHP_SELF']);
+?>
+
+<?php generateFooter(); ?>
