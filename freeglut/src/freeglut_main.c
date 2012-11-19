80b9498614c4a3ee36bef5dda3d48bb2138b3f56
dcnieho <dcnieho@7f0cb862-5218-0410-a997-914c9d46530a>
2012-11-19 07:16:45 +0000

    backporting r1349
    git-svn-id: https://svn.code.sf.net/p/freeglut/code/tags/FG_2_8_1_RC1@1429 7f0cb862-5218-0410-a997-914c9d46530a

--- freeglut/src/freeglut_main.c
+++ freeglut/src/freeglut_main.c
@@ -1656,7 +1656,8 @@ LRESULT CALLBACK fgWindowProc( HWND hWnd, UINT uMsg, WPARAM wParam,
         GetCursorPos( &mouse_pos );
         ScreenToClient( window->Window.Handle, &mouse_pos );
         hwnd = ChildWindowFromPoint(window->Window.Handle, mouse_pos);
-        window = fgWindowByHandle(hwnd);
+        if (hwnd)   /* can be NULL if mouse outside parent by the time we get here */
+            window = fgWindowByHandle(hwnd);
     }
 
     if ( window )
